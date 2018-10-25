832ca73af0d1b87f5dbe0e0c107d437780454971
Dane Wilson <git@danewilson.mefork@danewilson.me>
2018-10-25 00:56:30 -0400

    Added build

---
+++ assets/build/precache-manifest.ece6a39fa7ad9c9b6cf9a5a1df363456.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "541225fe61e4929a2008",
+    "url": "/static/js/main.541225fe.chunk.js"
+  },
+  {
+    "revision": "c97143232e3a3327ed9d",
+    "url": "/static/js/1.c9714323.chunk.js"
+  },
+  {
+    "revision": "541225fe61e4929a2008",
+    "url": "/static/css/main.10b666df.chunk.css"
+  },
+  {
+    "revision": "7942d98d092c66597d66668e31e99ca8",
+    "url": "/index.html"
+  }
+];
